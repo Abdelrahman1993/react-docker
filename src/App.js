7412740d73cf4a1d965102902dcc71ed39d36ac4
AbdElrahman <a.abdelfatah.100@gmail.com>
2020-08-06 02:27:43 +0200

    change app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HELLO THERE!!!!xxxxxxx
+          HELLO THERE FROM AWS
         </p>
         <a
           className="App-link"
